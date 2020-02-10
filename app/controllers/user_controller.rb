c47826dc159a302e678c018e06ec560861a5ca75
enzole1 <enzolp28@gmail.com>
2020-02-10 23:21:08 +0100

    v2

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,13 @@
 class UserController < ApplicationController
 
   def welcome_post
-    @first_name = params['first_name']
-    first_name_url = "/welcome/#{@first_name}"
+    user = User.create(first_name: params['first_name'], last_name: params['last_name'], email: params['email'], age: params['age'], city: City.create(name: params['name']), description: params['description'])
+    first_name_url = "/welcome/#{user.first_name}"
     redirect_to first_name_url
   end
 
   def welcome
+    @user_first_name = params['first_name']
     
   end
 
